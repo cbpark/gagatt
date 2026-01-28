9ef53986f965c89e463842ae95a4588c868be0f9
cbpark <cbpark@gmail.com>
2026-01-28 19:27:16 +0900

    Change epsabs and epsrel

--- src/integration.h
+++ src/integration.h
@@ -21,8 +21,8 @@ struct IntegralOffShell {
     double current_m1 = 0.0;
 
     // Error control
-    double epsabs = 1e-8;
-    double epsrel = 1e-5;
+    double epsabs = 1e-9;
+    double epsrel = 1e-6;
 };
 
 // double inner_m2_integrand(double m2, void *params);
@@ -32,7 +32,7 @@ struct IntegralOffShell {
 double integrateOffShellMasses(
     double sqrt_s_hat, double cos_th, double m_min,
     std::function<double(double, double, double, double)> func,
-    double epsrel = 1e-4);
+    double epsrel = 1e-6);
 }  // namespace gagatt
 
 #endif  // SRC_INTEGRATION_H
