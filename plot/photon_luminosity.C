b2ae89f6c85521d2d36a1887e5793127d7aaef19
cbpark <cbpark@gmail.com>
2026-02-04 21:51:59 +0900

    Update plot/photon_luminosity.C

--- plot/photon_luminosity.C
+++ plot/photon_luminosity.C
@@ -11,12 +11,20 @@ void photon_luminosity() {
     }
     file.close();
 
+    TGraph *gr = new TGraph(100, z, lumi);
+    gr->SetLineWidth(3);
+    gr->SetLineColor(kRed);
+    gr->SetLineStyle(10);
+    gr->SetTitle("");
+    gr->GetXaxis()->SetTitle("z = #sqrt{#tau}");
+    gr->GetYaxis()->SetTitle("1/L_{ee} dL_{#gamma #gamma }^{++}/dz");
+    gr->GetXaxis()->SetRangeUser(0.0, 1.0);
+    gr->GetYaxis()->SetRangeUser(0.0, 10.5);
+    gr->Draw("ALP");
+
     TGraph *gr_p = new TGraph(100, z, lumi_p);
     gr_p->SetLineWidth(3);
-    gr_p->SetTitle("");
-    gr_p->GetXaxis()->SetTitle("z = #sqrt{#tau}");
-    gr_p->GetYaxis()->SetTitle("1/L_{ee} dL_{#gamma #gamma }^{++}/dz");
-    gr_p->Draw("ACP");
+    gr_p->Draw("LP same");
 
     TGraph *gr_m = new TGraph(100, z, lumi_m);
     gr_m->SetLineWidth(3);
@@ -24,15 +32,9 @@ void photon_luminosity() {
     gr_m->SetLineStyle(9);
     gr_m->Draw("LP same");
 
-    TGraph *gr = new TGraph(100, z, lumi);
-    gr->SetLineWidth(3);
-    gr->SetLineColor(kRed);
-    gr->SetLineStyle(10);
-    gr->Draw("LP same");
-
     c1->Update();
     c1->GetFrame()->SetBorderSize(12);
     c1->Modified();
 
-    c1->SaveAs("photon_luminosity.pdf");
+    c1->SaveAs("photon_luminosity_pp.pdf");
 }
