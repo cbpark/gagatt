c40d49c4d9205c54486872729ef2e51a5945dc9e
cbpark <cbpark@gmail.com>
2026-02-04 23:28:37 +0900

    Simplify helicity module

--- src/helicity.h
+++ src/helicity.h
@@ -2,28 +2,14 @@
 #define SRC_HELICITY_H
 
 namespace gagatt {
-enum class Helicity { PLUS = 1, MINUS = -1, UNPOL = 0 };
+enum class Helicity : int { PLUS = 1, MINUS = -1, UNPOL = 0 };
 
 inline constexpr double toDouble(Helicity pol) {
-    switch (pol) {
-    case Helicity::PLUS:
-        return 1.0;
-    case Helicity::MINUS:
-        return -1.0;
-    default:
-        return 0.0;
-    }
+    return static_cast<double>(pol);
 }
 
-constexpr Helicity operator-(Helicity pol) {
-    switch (pol) {
-    case Helicity::PLUS:
-        return Helicity::MINUS;
-    case Helicity::MINUS:
-        return Helicity::PLUS;
-    default:
-        return pol;
-    }
+inline constexpr Helicity operator-(Helicity pol) {
+    return static_cast<Helicity>(-static_cast<int>(pol));
 }
 }  // namespace gagatt
 
