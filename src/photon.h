80a4fc977189fbec4d3ffdee9ed18dd4e3a13e12
cbpark <cbpark@gmail.com>
2026-02-20 18:40:28 +0900

    Fix photonLuminosityUnpol (for unpolarized beams)

--- src/photon.h
+++ src/photon.h
@@ -12,9 +12,11 @@ double photonLuminosity(double z, double x, double pe1, double pc1, double pe2,
                         double pc2, std::optional<Helicity> l1,
                         std::optional<Helicity> l2);
 
-inline double photonLuminosityUnpol(double z, double x, double pe1, double pc1,
-                                    double pe2, double pc2) {
-    return photonLuminosity(z, x, pe1, pc1, pe2, pc2, {}, {});
+// For unpoloarized initial beams (pe1 = pc1 = pe2 = pc2 = 0)
+// In this case, only contribution from <00> remains.
+// Note that, 0.25 * sum_{l1, l2} <00> = <00> with l1 = l2 = 0.
+inline double photonLuminosityUnpol(double z, double x) {
+    return photonLuminosity(z, x, 0.0, 0.0, 0.0, 0.0, {}, {});
 }
 }  // namespace gagatt
 
