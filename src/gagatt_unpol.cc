f737c8e9daf33b70802c23aadc55e95e822c5643
cbpark <cbpark@gmail.com>
2026-02-21 16:46:30 +0900

    Update gagatt_unpol (progress counter)

--- src/gagatt_unpol.cc
+++ src/gagatt_unpol.cc
@@ -4,15 +4,18 @@
 #include <iostream>
 #include "spin_density.h"
 
-// constexpr int N_COS = 10;
-// constexpr int N_SQRTS = 10;
+// constexpr int N_COS = 100;
+// constexpr int N_SQRTS = 50;
 constexpr int N_COS = 100;
 constexpr int N_SQRTS = 1000;
+constexpr int N_TOTAL = N_COS * N_SQRTS;
 
 constexpr double COS_TH_MIN = -1.0;
 constexpr double COS_TH_MAX = 1.0;
 constexpr double SQRTS_MIN = 346.0;  // just above 2 * m_t
 constexpr double SQRTS_MAX = 2000.0;
+constexpr double d_cos = (COS_TH_MAX - COS_TH_MIN) / N_COS;
+constexpr double d_sqrts = (SQRTS_MAX - SQRTS_MIN) / N_SQRTS;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -27,8 +30,6 @@ int main(int argc, char *argv[]) {
     }
     fout << "# cos(theta)\tsqrt(s_hat)\trho_t2\tconcurrence\ttr_c\tbell\n";
 
-    const double d_cos = (COS_TH_MAX - COS_TH_MIN) / N_COS;
-    const double d_sqrts = (SQRTS_MAX - SQRTS_MIN) / N_SQRTS;
     for (int i = 0; i < N_COS; ++i) {
         const double cos_th = COS_TH_MIN + (i + 0.5) * d_cos;
 
@@ -44,6 +45,10 @@ int main(int argc, char *argv[]) {
                                 gagatt::isEntangledByConcurrence(rho),
                                 gagatt::isEntangledByD(sdc),
                                 gagatt::violatesBellInequality(sdc));
+
+            if (const int np = i * N_SQRTS + j + 1; np % 1000 == 0) {
+                std::cout << std::format("-- progress: {} / {}\n", np, N_TOTAL);
+            }
         }
     }
 
