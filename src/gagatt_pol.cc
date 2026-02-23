e3280dfb08c8ba058e4984ebadd1246f92f57421
cbpark <cbpark@gmail.com>
2026-02-23 18:22:46 +0900

    Minor update

--- src/gagatt_pol.cc
+++ src/gagatt_pol.cc
@@ -1,3 +1,13 @@
+#include <fstream>
 #include <iostream>
 
-int main(int argc, char *argv[]) { std::cout << "Hello, world!\n"; }
+int main(int argc, char *argv[]) {
+    std::ofstream fout(argv[1]);
+    if (!fout) {
+        std::cerr << "Failed to open " << argv[1] << '\n';
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "gagatt_unpol: the output has been stored in " << argv[1]
+              << '\n';
+}
