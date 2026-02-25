89ebf46e05016fe055991048c9a4a8105d2e506a
cbpark <cbpark@gmail.com>
2026-02-25 16:36:17 +0900

    Update gagatt_pol_perfect

--- src/gagatt_pol_perfect.cc
+++ src/gagatt_pol_perfect.cc
@@ -2,24 +2,23 @@
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include "spin_density.h"
-
-constexpr int N_COS = 10;
-constexpr int N_SQRTS = 10;
-constexpr int N_TOTAL = N_COS * N_SQRTS;
 
 constexpr double COS_TH_MIN = -1.0;
 constexpr double COS_TH_MAX = 1.0;
 constexpr double SQRTS_MIN = 346.0;  // just above 2 * m_t
-constexpr double SQRTS_MAX = 500.0;
-constexpr double d_cos = (COS_TH_MAX - COS_TH_MIN) / N_COS;
-constexpr double d_sqrts = (SQRTS_MAX - SQRTS_MIN) / N_SQRTS;
+constexpr double SQRTS_MAX = 2000.0;
 
 using namespace gagatt;
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "usage: ./bin/gagatt_pol_perfect <output.dat>\n";
+    if (argc != 4) {
+        std::cerr
+            << "usage: ./bin/gagatt_pol_perfect <output.dat> N_COS N_SQRTS\n"
+            << "  <output.dat>: output file name.\n"
+            << "  N_COS: the size of grid for cos(Theta)\n"
+            << "  N_SQRTS: the size of grid for sqrt(s)\n";
         return EXIT_FAILURE;
     }
 
@@ -28,6 +27,12 @@ int main(int argc, char *argv[]) {
         std::cerr << "Failed to open " << argv[1] << '\n';
         return EXIT_FAILURE;
     }
+
+    const int N_COS = std::stoi(argv[2]);
+    const int N_SQRTS = std::stoi(argv[3]);
+    const int N_TOTAL = N_COS * N_SQRTS;
+    const double d_cos = (COS_TH_MAX - COS_TH_MIN) / N_COS;
+    const double d_sqrts = (SQRTS_MAX - SQRTS_MIN) / N_SQRTS;
 
     for (int i = 0; i < N_COS; ++i) {
         const double cos_th = COS_TH_MIN + (i + 0.5) * d_cos;
@@ -44,8 +49,10 @@ int main(int argc, char *argv[]) {
 
             auto rho_pp = spinDensityMatrix(sdc_pp);
 
-            fout << std::format("{:.2f}\t{:>12.4f}\t{}\n", cos_th, sqrt_s_hat,
-                                negativity(rho_pp));
+            fout << std::format(
+                "{:.2f}{:>12.4f}{:>8.4f}{:>8.4f}{:>8.4f}{:>9.4f}\n", cos_th,
+                sqrt_s_hat, negativity(rho_pp), getConcurrence(rho_pp),
+                horodeckiMeasure(sdc_pp), entanglementMarker(sdc_pp));
 
             if (const int np = i * N_SQRTS + j + 1; np % 1000 == 0) {
                 std::cout << std::format("-- progress: {} / {}\n", np, N_TOTAL);
