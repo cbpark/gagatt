5cb06a3f7a3305b2ca1b47f39bc1d537384549c0
cbpark <cbpark@gmail.com>
2026-02-26 11:24:03 +0900

    Update gagatt_pol

--- src/gagatt_pol.cc
+++ src/gagatt_pol.cc
@@ -2,6 +2,7 @@
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include "spin_density.h"
 
 constexpr int N_COS = 10;
@@ -10,14 +11,14 @@ constexpr int N_TOTAL = N_COS * N_SQRTS;
 
 constexpr double COS_TH_MIN = -1.0;
 constexpr double COS_TH_MAX = 1.0;
-constexpr double SQRTS_MIN = 346.0;  // just above 2 * m_t
-constexpr double SQRTS_MAX = 2000.0;
+constexpr double SQRT_TAU_MIN = 0.0;
+constexpr double SQRT_TAU_MAX = 1.0;
 constexpr double d_cos = (COS_TH_MAX - COS_TH_MIN) / N_COS;
-constexpr double d_sqrts = (SQRTS_MAX - SQRTS_MIN) / N_SQRTS;
+constexpr double d_sqrt_tau = (SQRT_TAU_MAX - SQRT_TAU_MIN) / N_SQRTS;
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "usage: ./bin/gagatt_pol <output.dat>\n";
+    if (argc != 3) {
+        std::cerr << "usage: ./bin/gagatt_pol <output.dat> <sqrt_s>\n";
         return EXIT_FAILURE;
     }
 
@@ -27,11 +28,16 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    const double sqrt_s = std::stod(argv[2]);
+    std::cout << "gagatt_pol: sqrt(s) = " << sqrt_s << '\n';
+
     for (int i = 0; i < N_COS; ++i) {
         const double cos_th = COS_TH_MIN + (i + 0.5) * d_cos;
 
         for (int j = 0; j < N_SQRTS; ++j) {
-            const double sqrt_s_hat = SQRTS_MIN + (j + 0.5) * d_sqrts;
+            // const double sqrt_s_hat = SQRTS_MIN + (j + 0.5) * d_sqrts;
+            const double sqrt_tau = 0.0 + (j + 0.5) * d_sqrt_tau;
+            const double sqrt_s_hat = sqrt_s * sqrt_tau;
 
             fout << std::format("{:.2f}\t{:>12.4f}\n", cos_th, sqrt_s_hat);
 
@@ -42,5 +48,5 @@ int main(int argc, char *argv[]) {
         fout << '\n';  // blank line = scanline separator for pm3d
     }
 
-    std::cout << "gagatt_pol: the output has been stored in " << argv[1] << '\n';
+    std::cout << "gagatt_pol: " << argv[1] << " written.\n";
 }
